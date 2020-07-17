66691987cbd074634d30415502bc3d5ae48fa652
swssr <simocmo4@gmail.com>
2020-07-17 10:41:31 +0200

    Clean up

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ScrollBar from "smooth-scrollbar";
+import AOS from "aos";
 
 import "./bgpattern.css";
 import "./style.css";
 
-ScrollBar.init(document.querySelector(".wrapper"))
+ScrollBar.init(document.querySelector(".wrapper"));
